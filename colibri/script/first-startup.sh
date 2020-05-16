fd2bcebb3674de58913b65024f7a12ed09303eb6
Colibri 63 <ange-marie@colibri-63.org>
2020-05-16 12:54:58 +0200

    Update first-startup.sh

--- colibri/script/first-startup.sh
+++ colibri/script/first-startup.sh
@@ -4,10 +4,10 @@ NEED_SETUP=$HOME/.need-desktop-setup
 
 if [ -f "$NEED_SETUP" ]; then
 	echo "setup needed"
-	chmod +x /home/colibriadmin/Bureau/*.desktop
-	gio set "/home/colibriadmin/Bureau/Sctratch Online.desktop" "metadata::trusted" "true"
-	gio set "/home/colibriadmin/Bureau/chrome.desktop" "metadata::trusted" "true"
-	gio set "/home/colibriadmin/Bureau/ENT Auvergne.desktop" "metadata::trusted" "true"
+	chmod +x /home/colibri/Bureau/*.desktop
+	gio set "/home/colibri/Bureau/Sctratch Online.desktop" "metadata::trusted" "true"
+	gio set "/home/colibri/Bureau/chrome.desktop" "metadata::trusted" "true"
+	gio set "/home/colibri/Bureau/ENT Auvergne.desktop" "metadata::trusted" "true"
 	gsettings set org.gnome.desktop.lockdown disable-lock-screen 'true'
 	rm $NEED_SETUP
 else
